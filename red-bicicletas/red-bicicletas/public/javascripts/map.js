68cc9836241e81876c470e8cc3b0da6ac97667de
Enrique Cazares <cazaresenrique97@gmail.com>
2019-09-11 12:19:13 -0500

    API, Fin semana 1

--- red-bicicletas/red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/red-bicicletas/public/javascripts/map.js
@@ -14,3 +14,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' , {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+//L.marker([19.3914038, -99.1858417]).addTo(mymap);
+
+$.ajax({
+	dataType: "json",
+	url: "api/bicicletas",
+	success: function(result){
+		console.log(result);
+		result.bicicletas.forEach(function(bici){
+			L.marker(bici.ubicacion, {title: bici.id}).addTo(mymap);
+		});
+	}
+})
